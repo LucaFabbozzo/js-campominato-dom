b4f2b58c763013b8c65d7595e9fb38ad84f7e894
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-21 05:34:05 -0500

    [add] new assignment

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-// **Bonus**
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
-
-
-// Macroaree;
-// 1 Creare la struttura html / css
-// 2 Creare la griglia dinamicamente
-// 3 Numerare i quadratini
-// 4 Gestire il click del quadratino
+// ****L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// **BONUS:**
+// 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// ****2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+// **Consigli del giorno:** :party_wizard:
+// ****Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+// Ad esempio:
+// Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 
 const container = document.querySelector('.container');
